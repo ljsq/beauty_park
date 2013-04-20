804fa27c6e36f24711f01a5d624190af04aba57a
Jian Li <lijian06nju@gmail.com>
2013-04-20 16:05:48 +0800

    Add display_message_activity, which accept message from main_activiay and display.

--- BeautyPark/src/li/xiaode/beautypark/MainActivity.java
+++ BeautyPark/src/li/xiaode/beautypark/MainActivity.java
@@ -1,10 +1,14 @@
 package li.xiaode.beautypark;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	public final static String EXTRA_MESSAGE = "li.xiaode.beautypark.MESSAGE";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,15 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void sendMessage(View view) {
+		// send message
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText)findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
 	}
 
 }
